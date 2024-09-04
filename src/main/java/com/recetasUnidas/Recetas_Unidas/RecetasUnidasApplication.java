becbfe640efc73c1f5a9b2bd0e13362d10f3cdeb
Edgar V. <vallan781@gmail.com>
2024-09-04 11:43:51 -0600

    Fist Commit

---
+++ src/main/java/com/recetasUnidas/Recetas_Unidas/RecetasUnidasApplication.java
@@ -0,0 +1,13 @@
+package com.recetasUnidas.Recetas_Unidas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecetasUnidasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecetasUnidasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recetasUnidas/Recetas_Unidas/RecetasUnidasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recetasUnidas.Recetas_Unidas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecetasUnidasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
